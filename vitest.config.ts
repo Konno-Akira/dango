ba0176b2d4f20e70b148d1f9d4e511026c226640
test <test-test.com>
2026-04-12 22:15:30 +0900

    rrrr

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'; //viteに変更
-// import react from '@vitejs/plugin-react'; //追加
-// import tailwindcss from '@tailwindcss/vite'; //追加
+import { defineConfig } from 'vitest/config'; //viteに変更
+//import react from '@vitejs/plugin-react'; //追加
+//import tailwindcss from '@tailwindcss/vite'; //追加
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  // plugins: [react(), tailwindcss()], //追加
+  plugins: [react(), tailwindcss()], //追加
   test: {
     include: [
       '**/*.spec.ts',
