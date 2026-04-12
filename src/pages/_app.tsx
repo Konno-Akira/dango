42c322f4db96875b7f00ba1a789d576386fbc953
test <test-test.com>
2026-04-13 01:12:57 +0900

    ttt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app.jsx';
 import React, { ReactElement } from 'react';
-@import "tailwindcss";
+// @import "tailwindcss";
 //import 'tailwindcss/tailwind.css';
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
