fad87969ebc4535df7a75dcfe9916c7daef60235
test <test-test.com>
2026-04-12 19:53:10 +0900

    tes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -141,7 +141,7 @@ const IndexPage: NextPage<Props> = ({ countries }: Props): ReactElement => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const buffer = await promises.readFile(join(process.cwd(), 'json', 'countries.json'));
+  const buffer = await promises.readFile('json/country.json'); // から変更←readFile(join(process.cwd(), 'json', 'countries.json'));
   const str = buffer.toString();
 
   return {
