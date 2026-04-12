538e646ecca3b49e0e04a78af5a87c1a6062c019
test <test-test.com>
2026-04-12 22:19:59 +0900

    rr

---
+++ tailwind.config.ts
@@ -0,0 +1,43 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//    "./src/pages/**/*.{js,ts,jsx,tsx}",
+//    "./src/components/**/*.{js,ts,jsx,tsx}",
+//    "./src/styles/**/*.{css}"
+//  ],
+//  safelist: [],
+//  darkMode: "media",
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//  variants: {
+//    backgroundColor: ["responsive", "hover", "focus", "active"],
+//    extend: {
+//      borderRadius: ["hover", "focus"],
+//      fontWeight: ["hover", "focus"],
+//    },
+//  },
+//};
+
+import type { Config } from 'tailwindcss';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+   content: [
+     './src/**/*.{js,jsx,ts,tsx}' から変更
+   ],
+   safelist: [],
+   darkMode: 'media',
+   theme: {
+     extend: {}
+   },
+   plugins: [],
+   variants: {
+     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+     extend: {
+       borderRadius: ['hover', 'focus'],
+       fontWeight: ['hover', 'focus']
+     }
+   }
+ } satisfies Config;
