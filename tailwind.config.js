2779bb7711942275d1787cca0b37ca640d14b81e
test <test-test.com>
2026-04-12 19:26:26 +0900

    teseter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,45 @@
-import type { Config } from 'tailwindcss';
-
-// eslint-disable-next-line import/no-default-export
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/styles/**/*.{css}"
-    // './src/**/*.{js,jsx,ts,tsx}' から変更
   ],
   safelist: [],
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
-    extend: {}
+    extend: {},
   },
   plugins: [],
   variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    backgroundColor: ["responsive", "hover", "focus", "active"],
     extend: {
-      borderRadius: ['hover', 'focus'],
-      fontWeight: ['hover', 'focus']
-    }
-  }
-} satisfies Config;
+      borderRadius: ["hover", "focus"],
+      fontWeight: ["hover", "focus"],
+    },
+  },
+};
+
+
+
+//import type { Config } from 'tailwindcss';
+
+// // eslint-disable-next-line import/no-default-export
+//export default {
+  // content: [
+    // './src/**/*.{js,jsx,ts,tsx}' から変更
+  // ],
+  // safelist: [],
+  // darkMode: 'media',
+  // theme: {
+    // extend: {}
+  // },
+  // plugins: [],
+  // variants: {
+    // backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    // extend: {
+      // borderRadius: ['hover', 'focus'],
+      // fontWeight: ['hover', 'focus']
+    // }
+  // }
+// } satisfies Config;
