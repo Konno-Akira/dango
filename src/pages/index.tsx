4702c79f5755762ab250741e9f096d2b406a0c22
test <test-test.com>
2026-04-12 21:10:19 +0900

    te

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,6 @@ const IndexPage: NextPage<Props> = ({ countries }: Props): ReactElement => {
     fetch('https://api.thecatapi.com/v1/images/search').then(async (res: Response) => {
       const json: Array<RandomCat> = await res.json() as Array<RandomCat>;
 
-      
       setCatImage(json[0]!);
     });
   }, []);
