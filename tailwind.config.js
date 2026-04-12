1c8d4b2382dc722cc9a7828c2164a4e374f6d4a7
test <test-test.com>
2026-04-12 19:21:40 +0900

    tester

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ import type { Config } from 'tailwindcss';
 // eslint-disable-next-line import/no-default-export
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{css}"
+    // './src/**/*.{js,jsx,ts,tsx}' から変更
   ],
   safelist: [],
   darkMode: 'media',
